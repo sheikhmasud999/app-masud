dac099ff0ed58ba14df480674b6ab2d6c3863287
sheikhmasud999 <wizesonat@gmail.com>
2021-10-03 10:52:24 +0600

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyanAccent,
+        centerTitle: true,
+        title: Text("Masud Sheikh"),
+      ),
+      body: Container(
+
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
